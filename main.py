a660d2bbd9decd08a53b45a934a1f6c30984c970
amine <amine@gmil.com>
2022-05-16 19:31:27 +0200

    transitivité

--- main.py
+++ main.py
@@ -2,28 +2,14 @@ import json
 import re
 import requests
 from bs4 import BeautifulSoup
+import os.path
 
-dictionary = {
-    "name": "sathiyajith",
-    "rollno": 56,
-    "cgpa": 8.6,
-    "phonenumber": "9976770500"
-}
 
 data = []
 
-
-def create_json_file(relation):
-    pass
+relatiuon_deductive = ['r_isa', 'r_holo']
 
 
-# create_json_file(relationentrante, relationsortante)
-
-"Tour Eiffel r_lieu France"
-
-
-def test(mot1, relation, mot2):
-    pass
 
 
 def getWordId(mot):
@@ -31,6 +17,14 @@ def getWordId(mot):
         for i in listedemot:
             liste = i.split(";")
             if mot == liste[1].replace("\"", ""):
+                return liste[0]
+
+
+def getRealationId(relation):
+    with open('idRel.txt') as listedemot:
+        for i in listedemot:
+            liste = i.split(";")
+            if str(relation) == str(liste[1].replace("\"", "").replace("\n","")):
                 return liste[0]
 
 
@@ -42,26 +36,17 @@ def getIdWord(id):
                 return liste[1].replace("\"", "")
 
 
-
-
-
-
-
-def extraction_relation(terme, relation):
+def extraction_relation_html(word_id, relation_id, word, relation):
     html = requests.get(
-        "http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" + terme + "&rel=" + relation)
+        "http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" + word + "&rel=" + relation)
     soup = BeautifulSoup(html.content, 'html.parser', from_encoding='iso-8859-1')
     texte_brut = soup.find_all('code')
     lignes_noeuds_et_relations = re.findall('[r];[0-9]*;.*', str(texte_brut))
-    print(len(lignes_noeuds_et_relations))
     counter = 0
-    filename = getWordId(terme)
     for r in lignes_noeuds_et_relations:
         splitedRelation = r.split(";")
-        # print(splitedRelation)
-        print(counter)
-        counter+=1
-        if splitedRelation[2] == str(filename):
+        counter += 1
+        if splitedRelation[2] == str(word_id):
             data.append(
                 {
                     "r": "sortante",
@@ -72,7 +57,7 @@ def extraction_relation(terme, relation):
                     "w": splitedRelation[5]
                 }
             )
-        elif splitedRelation[3] == str(filename):
+        elif splitedRelation[3] == str(word_id):
             data.append(
                 {
                     "r": "entrante",
@@ -84,17 +69,38 @@ def extraction_relation(terme, relation):
                 }
             )
 
-    with open('Cache/' + filename + '.json', 'w+') as output:
+    with open('Cache/' + relation_id + '/' + word_id + '.json', 'a+') as output:
         json.dump(data, output)
 
-# detectInference("Tour Eiffel","","France")
-#extraction_relation("Tour Eiffel", "r_lieu")
-#extraction_relation("France", "r_lieu")
 
-def detectInference(mot1, relation, mot2):
+def createCache(terme, relation):
+    word_id = getWordId(terme)
+    relation_id = getRealationId(relation)
+    if os.path.isdir('Cache/' + relation_id + '/'):
+        if os.path.isfile('Cache/' + relation_id + '/' + word_id + '.json'):
+            return
+        else:
+            extraction_relation_html(word_id, relation_id, terme, relation)
+    else:
+        os.mkdir('Cache/' + relation_id + '/')
+        extraction_relation_html(word_id, relation_id, terme, relation)
+
+
+
+
+
+def detectInfernce(mot1, relation, mot2):
+    createCache(mot1, relation)
+    createCache(mot2, relation)
+    transitive(mot1, relation, mot2)
+
+
+
+def transitive(mot1, relation, mot2):
     idmot1 = getWordId(mot1)
     idmot2 = getWordId(mot2)
-    with open('Cache/' + idmot1 + '.json') as relationmot1, open('Cache/' + idmot2 + '.json') as relationmot2:
+    idrelation = getRealationId(relation)
+    with open('Cache/' + idrelation +'/'+idmot1+ '.json') as relationmot1, open('Cache/' +idrelation+'/'+ idmot2 + '.json') as relationmot2:
         data = json.load(relationmot1)
         data2 = json.load(relationmot2)
         for i in data:
@@ -104,7 +110,26 @@ def detectInference(mot1, relation, mot2):
                         print(getIdWord(j["node1"]))
                         print(j["node1"])
 
-#extraction_relation("piston", "r_holo")
-#extraction_relation("voiture", "r_holo")
-#detectInference("piston","","voiture")
-print(getIdWord("5425"))
+
+# extraction_relation("Tour Eiffel", "r_lieu")
+# extraction_relation("France", "r_lieu")
+detectInfernce("Tour Eiffel","r_lieu","France")
+
+
+def deductive(mot1, relation, mot2):
+    idmot1 = getWordId(mot1)
+    idmot2 = getWordId(mot2)
+    idrelation = getRealationId(relation)
+    createCache(mot2, relation)
+    for i in relatiuon_deductive:
+        createCache(mot1,i)
+        deductive_relation_id =getRealationId(i)
+        with open('Cache/' + deductive_relation_id + '/' + idmot1 + '.json') as relationmot1, open('Cache/' + idrelation + '/' + idmot2 + '.json') as relationmot2:
+            data = json.load(relationmot1)
+            data2 = json.load(relationmot2)
+            for j in data:
+                pass
+
+
+
+
